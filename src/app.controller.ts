9867f82ff1d31189b9f9744e81fc39f21f17d741
pranav <pranavofficial404@gmail.com>
2022-11-26 16:52:10 +0530

    changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseFilters } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './filter';
 
+@UseFilters(new HttpExceptionFilter())
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    // throw Error();
     return this.appService.getHello();
   }
 }

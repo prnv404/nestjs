9faf60da130934f2fbd50b5c7ab01cab38cfcf3a
pranav <pranavofficial404@gmail.com>
2022-11-27 15:06:49 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
+import { SwaggerModule } from '@nestjs/swagger';
+import { createDocument } from './swagger/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(morgan('dev'));
-  await app.listen(3000);
+   const app = await NestFactory.create(AppModule);
+   app.setGlobalPrefix('api/v1');
+   SwaggerModule.setup('api', app, createDocument(app));
+   app.use(morgan('dev'));
+   await app.listen(3000);
 }
 bootstrap();
---
+++ src/swagger/swagger.config.ts
@@ -0,0 +1,8 @@
+import { SwaggerConfig } from './swagger.interface';
+
+export const SWAGGER_CONFIG: SwaggerConfig = {
+   title: 'Nestjs tutorial',
+   description: 'Some random api ',
+   version: '1.0.0',
+   tags: ['Templates'],
+};
